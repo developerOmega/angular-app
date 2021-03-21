f426a83ab74cee6d9feb8fb98f959663a2ab021a
developerOmega <developerOmega@developerOmega.com>
2021-03-21 17:38:51 -0600

    Documentar assets

--- src/assets/cookies.ts
+++ src/assets/cookies.ts
@@ -1,18 +1,26 @@
 export default class Cookie {
  
+  // Método que agrega una una nueva cookie
+  // Recibir parámetros -> cmane:string, cvalue:string
   static setCookie(cname:string, cvalue:string){
     document.cookie = `${cname} = ${cvalue};`;
   }
 
+  // Método que busca una cookie
+  // Recibir parámetros -> cmane:string
   static getCookie(cname:string):string {
-    const decodedCookie = decodeURIComponent(document.cookie);  
+    // Definir una constante que almacena las cookies
+    const decodedCookie = decodeURIComponent(document.cookie);
+    // Separar las cookies por ;
     const cookies = decodedCookie.split(';');
   
+    // Filtrar la cooke que sea igual al parámetro cname
     const data = cookies.filter( cookie => {
       const cooArr = cookie.split("=");
       return cooArr[0] == cname;
     });
   
+    // Si existe un cookie, retornar valor de cookie
     if(data[0]) {
       return data[0].split("=")[1];
     }
@@ -20,27 +28,32 @@ export default class Cookie {
     return "";
   }
 
+  // Método que busca una cookie de tipo json
+  // Recibir parámetros -> cmane:string
   static getCookieJSON(cname:string):Object {
     const data = Cookie.getCookie(cname);
     return JSON.parse(data);
   }
 
+  // Método que busca una cookie
+  // Recibir parámetros -> cmane:string
   static deleteCookie(cname:string) {
-    const decodedCookie = decodeURIComponent(document.cookie);  
+    // Definir una constante que almacena las cookies
+    const decodedCookie = decodeURIComponent(document.cookie);
+    // Separar las cookies por ;
     const cookies = decodedCookie.split(';');
-  
+    
+     // Eliminar el valor de la cookie que sea igual al parámetro cname
     let data = cookies.map( cookie => {
       const cooArr = cookie.split("=");
       return cooArr[0] == cname ? `${cname}=` : cookie;
     });
     
+    // Unir las cookies por ;
     let newCookie = data.join(';');
     
-    console.log(newCookie);
-    console.log(document.cookie);
-    
     document.cookie = newCookie ;
-  
+
   }
 
 }
--- src/assets/request.ts
+++ src/assets/request.ts
@@ -1,23 +1,37 @@
 import Cookie from '../assets/cookies';
 
 export default class Request {
+  // Propiedad de url
   url: string = "";
+
+  // Propiedad del token jwt
   token: string = "";
+
   constructor() {
     this.url = 'http://localhost:3000';
     this.token = Cookie.getCookie('token') 
   }
 
+  // Método que hace una petición get para buscar una colección
+  // Recibir parámetros -> route:string (Ruta de la petición)
   public async get( route:string = "" ) {
+    // Url para para crear petición
     const url = this.url + route;
+    
+    // Hacer petición
     const response = await fetch(url);
     const data = await response.json();
 
     return data;
   }
 
+  // Método que hace una petición post para agregar columna a una colección
+  // Recibir parámetros -> route:string (Ruta de la petición), body:object (Datos de la columna), isSession:boolean (Es necesario el token jwt)
   public async post( route:string = "", body:object, isSession = true ){
+    // Url para para crear petición
     const url = this.url + route;
+
+    // Configuración de petición
     const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(body),
@@ -25,15 +39,20 @@ export default class Request {
         'Content-Type': 'application/json',
         'Authorization': isSession ? this.token : ""
       }
-
     });
+    // Hacer petición
     const data = await response.json();
 
     return data;
   }
 
+  // Método que hace una petición put para editar la columna de una colección
+  // Recibir parámetros -> route:string (Ruta de la petición), body:object (Datos de la columna)
   public async put(route:string = "", body:object) {
+    // Url para para crear petición
     const url = this.url + route;
+
+    // Configuración de petición
     const response = await fetch(url, {
       method: 'PUT',
       body: JSON.stringify(body),
@@ -42,13 +61,20 @@ export default class Request {
         'Authorization': this.token
       }
     });
+
+    // Hacer petición
     const data = await response.json();
 
     return data;
   }
 
+  // Método que hace una petición delete para eliminar la columna de una colección
+  // Recibir parámetros -> route:string (Ruta de la petición), body:object (Datos de la columna)
   public async delete(route:string = "") {
+    // Url para para crear petición
     const url = this.url + route;
+
+    // Configuración de petición
     const response = await fetch(url, {
       method: 'DELETE',
       headers:{
@@ -56,6 +82,8 @@ export default class Request {
         'Authorization': this.token
       }
     });
+
+    // Hacer petición
     const data = await response.json();
 
     return data;
